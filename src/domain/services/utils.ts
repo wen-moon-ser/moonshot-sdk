4e1c6952c40f3d41f520c992f0d8ffaf38d56414
ibrica <ivbritvic@gmail.com>
2024-11-12 17:34:20 +0100

    feat: curve types

--- src/domain/services/utils.ts
+++ src/domain/services/utils.ts
@@ -1,26 +1,3 @@
-export const imagePathToBase64 = (input: string | File): Promise<string> => {
-  return new Promise((resolve, reject) => {
-    if (typeof window !== 'undefined' && input instanceof File) {
-      // Browser environment
-      const reader = new FileReader();
-      // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-      reader.onloadend = () => {
-        resolve(reader.result as string);
-      };
-      reader.onerror = reject;
-      reader.readAsDataURL(input);
-    } else if (typeof input === 'string') {
-      // Node.js environment
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
-      const { readFileSync } = require('fs');
-      try {
-        const imageBuffer = readFileSync(input);
-        resolve(imageBuffer.toString('base64'));
-      } catch (error) {
-        reject(error);
-      }
-    } else {
-      reject(new Error('Invalid input type'));
-    }
-  });
+export const imagePathToBase64 = (input: string): string => {
+  return Buffer.from(input).toString('base64'); // TODO: check how to do file without fs so it is browser compatible
 };
