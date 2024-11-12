cc4c7c39fe56cd4e20c931aa7c1aa391048aa0e7
ibrica <ivbritvic@gmail.com>
2024-11-12 16:45:43 +0100

    fix: remove fs so it works in the browser

--- src/domain/services/utils.ts
+++ src/domain/services/utils.ts
@@ -1,7 +1,26 @@
-import { readFileSync } from 'fs';
-
-export const imagePathToBase64 = (filePath: string): string => {
-  const imageBuffer = readFileSync(filePath);
-
-  return imageBuffer.toString('base64');
+export const imagePathToBase64 = (input: string | File): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    if (typeof window !== 'undefined' && input instanceof File) {
+      // Browser environment
+      const reader = new FileReader();
+      // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+      reader.onloadend = () => {
+        resolve(reader.result as string);
+      };
+      reader.onerror = reject;
+      reader.readAsDataURL(input);
+    } else if (typeof input === 'string') {
+      // Node.js environment
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const { readFileSync } = require('fs');
+      try {
+        const imageBuffer = readFileSync(input);
+        resolve(imageBuffer.toString('base64'));
+      } catch (error) {
+        reject(error);
+      }
+    } else {
+      reject(new Error('Invalid input type'));
+    }
+  });
 };
